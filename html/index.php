6c612880eeb65c6016e3dc3e54201f72f140e07f
Märta Holmquist <marta.holmquist.1038@student.lu.se>
2023-11-08 14:24:32 +0100

    initialize

---
+++ html/index.php
@@ -0,0 +1,11 @@
+<?php 
+    session_start();
+    $currpage = "index.php";
+?>
+
+<html>
+    <head>
+        <h1> Buy drugs legally online!! </h1>
+        <link rel ="stylesheet" href="css/index.css"/>
+    </head>
+</html>
