eff088f22e7745e03215c8e46f27b6eeb944b99a
Thilda Holmner <thilda.holmner.8080@student.lu.se>
2023-12-13 16:28:29 +0100

    adds total displayed in checkout page

--- html/config.php
+++ html/config.php
@@ -1,7 +1,8 @@
 <?php
 
-session_start();
-
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 if (!isset($_SESSION['csrf_token'])) {
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32)); 
 }
